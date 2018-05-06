b4ff9826351213da34e4de4e0bf9466e0028eeaa
github username <github email address>
2018-05-06 19:25:15 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
-class Student 
+class Student < User
+
+# CONFUSED ABOUT WHY THIS CLASS HAS A :KNOWLEDGE AA, BUT TEACHER DOESN'T
+
+attr_accessor :knowledge
+
+def initialize
+    @knowledge = []
+  end
+
+
+# takes in a string and adds that string to the student's `@knowledge` array
+  def learn(string)
+    @knowledge << string
+  end
+
+
+# returns that student's knowledge array
+  def knowledge
+    @knowledge
+  end
 
 end
